db7e4c51842b0ebcc79a595dfa6ae67090faea98
hsezhiyan <40924035+hsezhiyan@users.noreply.github.com>
2018-09-18 16:26:43 -0700

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([4, 5, 6, 7, 8], [-5.69932, -6.6792, -7.66483, -8.64906, -9.62424], 'ro')
+plt.title('Log-Log Approximation Errors at x = 1/2, t = 1')
+plt.ylabel('Log Error')
+plt.xlabel('p values')
+plt.show()
